bc4d0fa61a80e3d33ada0bf1d7fe7c2bcf6bf75b
Color-Kat <50361931+KatSem7@users.noreply.github.com>
2021-04-03 14:15:14 +0300

    'some'

--- app/Http/Controllers/api/BirdsController.php
+++ app/Http/Controllers/api/BirdsController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\BirdsRequest;
 use App\models\Bird;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BirdsController extends Controller
 {
@@ -17,7 +18,7 @@ class BirdsController extends Controller
      */
     public function index()
     {
-
+dd(Auth::user());
         return Bird::all();
     }
 
--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 Route::resource('birds', 'api\BirdsController');
+
+//Route::group([
+//
+//    'prefix'     => 'auth',
+//    'namespace'  => 'Auth'
+//
+//], function ($router) {
+//
+//    Route::post('login', 'LoginController')->name('login');
+//    Route::post('logout', 'LogoutController')->name('logout');
+//
+//    Route::get('account', 'AccountController')->name('account');
+//});
+
+
+
+
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix'     => 'auth',
+
+], function ($router) {
+    Route::post('register', 'AuthController@register');
+    Route::post('login', 'AuthController@login')->name('login');
+    Route::get('logout', 'AuthController@logout');
+    Route::get('account', 'AuthController@account');
+
+});
+
