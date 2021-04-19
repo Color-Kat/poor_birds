172de9970cefae36d2d019186b37eca5ebf95a1b
Color-Kat <katik7979@gmail.com>
2021-04-19 21:32:18 +0300

    some rules and new eggs table migration

---
+++ database/migrations/2021_04_19_182835_create_eggs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEggsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eggs', function (Blueprint $table) {
+            $table->id();
+            $table->integer('price'); // цена одного яйца
+            $table->integer('demand'); // спрос - сколько за час можно продавать яиц
+            $table->integer('count'); // кол-во яиц
+            $table->foreignId('user_id');
+            $table->foreignId('bird_seller_id'); // чтобы различать яйца разных птиц
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eggs');
+    }
+}
