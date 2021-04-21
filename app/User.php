a9760da39add7de7e87d21882a8064b7be427b5a
Color-Kat <katik7979@gmail.com>
2021-04-21 16:26:46 +0300

    Changed get_my_birds_with_certificate in User with using Bird::apply_certificate_to_bird

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\models\Bird;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -39,33 +40,38 @@ class User extends Authenticatable implements JWTSubject
     {
         $my_bird_with_certificate = [];
 
-        foreach ($my_birds as $my_bird) {
+        foreach ($my_birds as $key => $my_bird ) {
             $bird        = $my_bird->bird;
             $certificate = $my_bird->seller->certificate;
 
-            // certificate bonuses
-            $fertility_bonus = $certificate ? 1 + $certificate->fertility_bonus / 100 : 1;
-            $demand_bonus    = $certificate ? 1 + $certificate->demand_bonus / 100: 1;
-            $care_bonus      = $certificate ? 1 + $certificate->care_bonus / 100: 1;
-            $litter_bonus    = $certificate ? 1 + $certificate->litter_bonus / 100: 1;
-            $price_bonus     = $certificate ? 1 + $certificate->price_bonus / 100: 1;
+            $my_bird_with_certificate[$key] = Bird::apply_certificate_to_bird($bird, $certificate);
+            $my_bird_with_certificate[$key]["id"] = $my_bird->id;
+            $my_bird_with_certificate[$key]["bird_seller_user_id"] = $my_bird->pivot->id;
+            $my_bird_with_certificate[$key]["count"] = $my_bird->pivot->count;
 
-            $my_bird_with_certificate[] = [
-                "id"                  => $my_bird->id,
-                "image"               => $bird->image,
-                "name"                => $bird->name,
-                "description"         => $bird->description,
-                "price"               => $bird->price,
-                "fertility"           => round($bird->fertility * $fertility_bonus),
-                "demand"              => round($bird->demand * $demand_bonus),
-                "care"                => round($bird->care * $care_bonus),
-                "litter"              => round($bird->litter * $litter_bonus),
-                "egg_price"           => round($bird->egg_price * $price_bonus),
-                "count"               => $my_bird->pivot->count,
-                "certificate_id"      => $certificate ? $certificate->id : 0,
-                "bird_seller_user_id" => $my_bird->pivot->id,
-                "pivot"               => $my_bird->pivot
-            ];
+//            // certificate bonuses
+//            $fertility_bonus = $certificate ? 1 + $certificate->fertility_bonus / 100 : 1;
+//            $demand_bonus    = $certificate ? 1 + $certificate->demand_bonus / 100: 1;
+//            $care_bonus      = $certificate ? 1 + $certificate->care_bonus / 100: 1;
+//            $litter_bonus    = $certificate ? 1 + $certificate->litter_bonus / 100: 1;
+//            $price_bonus     = $certificate ? 1 + $certificate->price_bonus / 100: 1;
+//
+//            $my_bird_with_certificate[] = [
+//                "id"                  => $my_bird->id,
+//                "image"               => $bird->image,
+//                "name"                => $bird->name,
+//                "description"         => $bird->description,
+//                "price"               => $bird->price,
+//                "fertility"           => round($bird->fertility * $fertility_bonus),
+//                "demand"              => round($bird->demand * $demand_bonus),
+//                "care"                => round($bird->care * $care_bonus),
+//                "litter"              => round($bird->litter * $litter_bonus),
+//                "egg_price"           => round($bird->egg_price * $price_bonus),
+//                "count"               => $my_bird->pivot->count,
+//                "certificate_id"      => $certificate ? $certificate->id : 0,
+//                "bird_seller_user_id" => $my_bird->pivot->id,
+//                "pivot"               => $my_bird->pivot
+//            ];
         }
 
         return $my_bird_with_certificate;
