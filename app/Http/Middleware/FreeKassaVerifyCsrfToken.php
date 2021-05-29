73aed353280f02b9411a748a758b825711051681
Color-Kat <katik7979@gmail.com>
2021-05-29 16:25:08 +0300

    freekassa csrf whitelist

---
+++ app/Http/Middleware/FreeKassaVerifyCsrfToken.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Illuminate\Support\Facades\Log;
+
+class FreeKassaVerifyCsrfToken extends Middleware
+{
+
+    private $whiteList = [
+        '136.243.38.147',
+        '136.243.38.149',
+        '136.243.38.150',
+        '136.243.38.151',
+        '136.243.38.189',
+        '136.243.38.108',
+        '136.243.38.*',
+//        '127.0.0.1'
+    ];
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        Log::info('123');
+        if (!in_array($request->ip(), $this->whiteList)) {
+            abort(403);
+        }
+        return $next($request);
+    }
+}
