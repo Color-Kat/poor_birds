435da83b8bd299f5d811a749bf60e415770b5db2
Color-Kat <katik7979@gmail.com>
2021-06-18 21:28:20 +0300

    ts support
    (cherry picked from commit 3cb1fd894ea702c38cb5f97107d91f96c7e78f11)

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -4,26 +4,30 @@ import router from './router';
 import store from './store';
 
 /* bootstrap-vue */
-import {BootstrapVue, IconsPlugin} from 'bootstrap-vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import './app.scss';
 
 /* nprogress - progress bar   */
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
 
+// import some from './modules/request';
+
+// console.log(some);
+
 // any properties in window
 declare const window: any;
 
 /* ----- configure axios for server ----- */
-window.axios = axios;
-axios.defaults.baseURL = 'http://127.0.0.1:8000';
+// window.axios = axios;
+// axios.defaults.baseURL = 'http://127.0.0.1:8000';
 // window.axios = axios.create({ baseURL: 'https://poorbirds.rf.gd', timeout: 100000, });
 // window.axios = axios.create({ baseURL: 'https://s367343.smrtp.ru', timeout: 100000, });
-// window.axios = axios.create({baseURL: 'https://poorbirds.tk', timeout: 100000,});
+window.axios = axios.create({ baseURL: 'https://poorbirds.tk', timeout: 100000, });
 /* ----- end configure ----- */
 
 /* ------- NPROGRESS -------- */
-NProgress.configure({easing: 'ease', speed: 500});
+NProgress.configure({ easing: 'ease', speed: 500 });
 // before a request is made start the nprogress
 window.axios.interceptors.request.use(config => {
     NProgress.start();
@@ -34,14 +38,13 @@ window.axios.interceptors.response.use(response => {
     NProgress.done();
     return response;
 });
-NProgress.configure({showSpinner: false}); // disable spinner
+NProgress.configure({ showSpinner: false }); // disable spinner
 /* ------- END NPROGRESS -------- */
 
 /* --------------------------------------- */
 /* ------- REGISTER SERVICE-WORKER ------- */
 /* --------------------------------------- */
 import {initSW} from "./enable-push";
-
 window.addEventListener("load", () => {
     initSW();
 });
@@ -50,7 +53,6 @@ window.addEventListener("load", () => {
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 
-// register App component
 Vue.component('app', require('./App.vue').default);
 
 const app = new Vue({
