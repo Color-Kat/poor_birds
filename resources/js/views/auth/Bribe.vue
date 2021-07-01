b6d86f9ddbcf887897a5fbbc82899b17615a516f
Color-Kat <katik7979@gmail.com>
2021-07-01 15:01:16 +0300

    BRIBE and Gustov

--- resources/js/views/auth/Bribe.vue
+++ resources/js/views/auth/Bribe.vue
@@ -7,12 +7,62 @@
             А всё потому, что Густов в любой момент может вас снова попытаться убить.
             Поэтому вы решаете дать ему большущую взятку в размере 1.000.000.000 рублей.
         </p>
+        <hr>
+
+        <div class="d-flex justify-content-center flex-wrap">
+            <div class="w-50 ">
+
+                <img class="w-100" src="http://www.procuror.spb.ru/gif/gustov.jpg" alt="ГУСТОВ">
+
+                <div class="d-flex justify-content-center flex-wrap">
+                    <b-button class="w-100" variant="danger" @click="repayLoan">💰Отдать все свои деньги💰</b-button>
+                    <span class="alert-info p-2 m-2 badge-pill">Осталось: {{rest}}₽</span>
+                </div>
+            </div>
+        </div>
     </b-card>
 </template>
 
 <script>
+import {mapActions} from "vuex";
+
 export default {
-    name: "Bribe"
+    name   : "Bribe",
+    data   : () => ({
+        rest: this.bribeRest
+    }),
+    computed: {
+        ...mapActions(['bribeRest'])
+    },
+    methods: {
+        repayLoan() {
+            this.rest -= 100000;
+
+            if (this.rest >= 5000000) {
+                // sound of damage
+                let song    = new Audio( '/assets/sounds/death.mp3');
+                song.volume = 0.5;
+                song.play();
+            } else if (this.rest <= 0) {
+                // sound of dying
+                let song    = new Audio('/assets/sounds/groans.mp3');
+                song.volume = 0.5;
+                song.play();
+
+                song.onended = () => {
+                    // sound of death
+                    let song2 = new Audio('/assets/sounds/death.mp3');
+                    song2.volume = 0.5;
+                    song2.play();
+                }
+            } else if (this.rest < 5000000) {
+                // sound of heart beat
+                let song    = new Audio('/assets/sounds/heart_beat.mp3');
+                song.volume = 0.5;
+                song.play();
+            }
+        }
+    }
 }
 </script>
 
